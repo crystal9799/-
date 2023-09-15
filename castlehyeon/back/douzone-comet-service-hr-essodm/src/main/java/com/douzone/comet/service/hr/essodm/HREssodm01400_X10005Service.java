69958f193baea1607418baa9df1a3dd3a588a028
김성현 <qnftktls118@naver.com>
2023-09-15 15:54:09 +0900

    feat: Service 복원

--- castlehyeon/back/douzone-comet-service-hr-essodm/src/main/java/com/douzone/comet/service/hr/essodm/HREssodm01400_X10005Service.java
+++ castlehyeon/back/douzone-comet-service-hr-essodm/src/main/java/com/douzone/comet/service/hr/essodm/HREssodm01400_X10005Service.java
@@ -34,18 +34,22 @@ public class HREssodm01400_X10005Service extends DzCometService {
 	public List<Essodm01400_X10005Model> list_HR_OFFAPPLY_MST_X10005(
 		@DzParam(key="start_dt", desc="시작일", paramType = DzParamType.QueryString) String start_dt,
 		@DzParam(key="end_dt", desc="종료일", paramType = DzParamType.QueryString) String end_dt,
-		@DzParam(key="company_cd", desc="회사코드", paramType = DzParamType.QueryString) String company_cd,
-		@DzParam(key="emp_no", desc="사원번호", paramType = DzParamType.QueryString) String emp_no
+		@DzParam(key="company_cd", desc="회사코드", paramType = DzParamType.QueryString) String company_cd
 	) throws Exception {
 		List<Essodm01400_X10005Model> items =  new ArrayList<Essodm01400_X10005Model>();	
 		try {
 			HashMap<String, Object> parameters = new HashMap<>();
-	 		parameters.put("P_EMP_NO", emp_no);
 			parameters.put("P_COMPANY_CD", company_cd);
 			parameters.put("P_START_DT", start_dt);
 			parameters.put("P_END_DT", end_dt);
 	 		
 	 		items = essodm01400_x10005DAO.selectEssodm01400_X10005ModelList(parameters);
+	 		
+	 		System.out.println("MainGrid1Model==========================");
+			for (Essodm01400_X10005Model item : items) {
+				System.out.println(item.toString());
+			}
+			System.out.println("MainGrid1Model==========================");
 	 	} catch(Exception e) {
 	        throw new DzApplicationRuntimeException(e);
 	    }
@@ -53,14 +57,17 @@ public class HREssodm01400_X10005Service extends DzCometService {
 	    return items;
  	}
  	
+ 	//이건 사용하지 않을 것 그리드에서 기본정보를 다 들고 있음.
  	@DzApi(url="/getUserInfo", desc="메인페이지 기본정보 조회", httpMethod=DzRequestMethod.GET)
 	public List<Essodm01400_X10005_UserInfoModel> getUserInfo(
+	@DzParam(key="company_cd", desc="회사코드", paramType = DzParamType.QueryString) String company_cd,
+	@DzParam(key="emp_no", desc="사원번호", paramType = DzParamType.QueryString) String emp_no
 	) throws Exception {
 		Essodm01400_X10005_UserInfoModel item =  new Essodm01400_X10005_UserInfoModel();
 		List<Essodm01400_X10005_UserInfoModel> items = new ArrayList<Essodm01400_X10005_UserInfoModel>();
 		//현재 접속자 정보를 가져오지 않고 가장 많은 건수를 가진 사원정보를 입력
-		String company_cd = "EWP";
-		String emp_no = "20200308";
+//		String company_cd = "EWP";
+//		String emp_no = "20200308";
 		try {
 	 		HashMap<String, Object> parameters = new HashMap<>();
 	 		parameters.put("P_EMP_NO", emp_no);

94fddf195f0bfe0cc6baf2cff2a43e7d3e25346b
김성현 <qnftktls118@naver.com>
2023-09-29 19:56:57 +0900

    feat: 검색조건에 따라 서비스명, 서비스설명, url, url설명 또는 전체 검색이 가능하도록 구현
    - 전체검색 일때는 텍스트 박스 값 초기화
    - 검색 결과가 없을 시 state가 success여도 json 백업파일에 저장되지 않도록 구현
    - 로컬스토리지, json백업파일 둘 다 전체검색시에만 저장되도록 로직 수정

--- castlehyeon/back/douzone-comet-service-hr-dwagger/src/main/java/com/douzone/comet/service/hr/dwagger/DwaggerService.java
+++ castlehyeon/back/douzone-comet-service-hr-dwagger/src/main/java/com/douzone/comet/service/hr/dwagger/DwaggerService.java
@@ -31,73 +31,35 @@ public class DwaggerService extends DzCometService {
 	@Autowired
 	JsonUtils jsonUtils;
 
-	// 사용자 입력값에 따라 결과를 받아오는 api
-	@DzApi(url = "/dwagger_list", desc = "서비스조회-서비스명", httpMethod = DzRequestMethod.GET)
-	public List<DwaggerModel> dwagger_list(
-			@DzParam(key = "serviceName", desc = "서비스명", paramType = DzParamType.QueryString) String serviceName,
-			@DzParam(key = "langCd", desc = "언어", paramType = DzParamType.QueryString) String langCd) throws Exception {
-
-		List<DwaggerModel> dwaggerModelList = new ArrayList<DwaggerModel>();
-		APIDataCall callDatas = new APIDataCall();
-		
-		System.out.println("serviceName===>"+serviceName);
-		System.out.println("langCd===>"+langCd);
-		try {
-			// 파라미터로 받은 serviceName과 일치하는 DwaggerModel을 찾기 위한 변수
-			DwaggerModel foundModel = null;
-			
-			String result = callDatas.callApi("");
-			
-			//결과값을 찾을 수 있다면
-			// 등록서비스 조회 api의 state가 error가 아니라면
-			if (!result.equals("-1")) {
-				dwaggerModelList = jsonUtils.convertJsonToObject(result);
-			}else if(serviceName != null && serviceName.equals("")) {
-				// APICall을 사용하는 것이 아닌 기존 빈등록된 객체 정보 내에서 값을 찾도록 로직 작성
-				// 기존 cacheModels 객체에서 로직에 따라 데이터 조회
-				// 로직작성
-				// cacheModels 객체의 apiList에서 serviceName과 일치하는 DwaggerModel 찾기
-				for (DwaggerModel model : cacheModels.getApiList()) {
-					if (model.getService().equals(serviceName)) {
-						foundModel = model;
-						dwaggerModelList.add(foundModel);
-					}
-				}
-			}
-
-			/////
-		} catch (Exception e) {
-			throw new DzApplicationRuntimeException(e);
-		}
-
-		return dwaggerModelList;
-	}
-
-	// 초기에 전체 서비스를 받아오는 api
+	//서비스를 받아오는 api
 	@DzApi(url = "/dwagger_totalList", desc = "서비스조회-서비스명", httpMethod = DzRequestMethod.GET)
 	public List<DwaggerModel> dwagger_totalList(
-			@DzParam(key = "serviceName", desc = "서비스명", paramType = DzParamType.QueryString) String serviceName,
-			@DzParam(key = "langCd", desc = "언어", paramType = DzParamType.QueryString) String langCd) throws Exception {
+			@DzParam(key = "keywords", desc = "검색어", paramType = DzParamType.QueryString) String keywords,
+			@DzParam(key = "langCd", desc = "언어", paramType = DzParamType.QueryString) String langCd,
+			@DzParam(key = "condition_cd", desc = "검색조건코드", paramType = DzParamType.QueryString) String condition_cd) throws Exception {
 		APIDataCall callDatas = new APIDataCall();
 		List<DwaggerModel> dwaggerModelList = new ArrayList<DwaggerModel>();
 		try {
 			System.out.println("Service cacheModels===>" + cacheModels);
-			String result = callDatas.callApi("");
+			String result = callDatas.callApi(keywords, condition_cd);
 
-//어째서인지 자바 객체를 굳이 JSON으로 직렬화하지 않아도 올바르게 데이터가 전송된다. 아마 DzApi가 RestController로 작동하기 때문일 듯.
+			//어째서인지 자바 객체를 굳이 JSON으로 직렬화하지 않아도 올바르게 데이터가 전송된다. 아마 DzApi가 RestController로 작동하기 때문일 듯.
+			//결과값을 찾을 수 있다면
+			// 등록서비스 조회 api의 state가 error가 아니라면
 			if (!result.equals("-1")) {
 				dwaggerModelList = jsonUtils.convertJsonToObject(result);
 				
 				// 변환된 자바 객체를 서버메모리에 저장하기 위해 set사용.
 				// 새로 값을 불러온다면 이를 파일로 저장.
-				if (cacheModels != null) {
+				// 반드시 전체조건검색일 때만 백업파일로 저장하기
+				if (cacheModels != null && condition_cd.equals("1")) {
 					cacheModels.setApiList(dwaggerModelList);
 					cacheModels.setUpdateDTS(cacheModels.getDate());
+					jsonUtils.saveCacheDwaggerModelsToFile();
 				}
 				System.out.println("--------------------------");
 				System.out.println("cacheModels==>" + cacheModels);
 				System.out.println("--------------------------");
-				jsonUtils.saveCacheDwaggerModelsToFile();
 			}
 
 		} catch (Exception e) {
